17c66fed53dab2751e22fcec28da7750007c5304
Kostya <kostya173russ@gmail.com>
2023-06-26 22:28:17 +0300

    решены все задачи

---
+++ Task2/Program.cs
@@ -0,0 +1,78 @@
+﻿//  Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите размер массива m x n");
+int m = Prompt("Введите m: ");
+int n = Prompt("Введите n: ");
+int[,] array = new int[m, n];
+GetArray(array);
+WriteArray(array);
+
+int minSumLine = 0;
+int sumLine = SumOfLine(array, 0);
+for (int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumOfLine(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой ({sumLine}) элементов ");
+
+
+int SumOfLine (int[,] array, int i)
+{
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
+}
+
+int Prompt (string message) 
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int val = 0;
+    while ((int.TryParse(value, out val)) != true)
+    {
+        Console.Write("Неверный ввод. Повторите: ");
+        value = Console.ReadLine();
+
+    }
+
+    return val;
+}
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int rows = Prompt("Введите количество строк: ");
+int columns = Prompt("Введите количество столбцов: ");
+int[,] firstmatrix = new int[rows, columns];
+int[,] secondmatrix = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
+
+Console.WriteLine();
+
+Console.WriteLine("Первая матрица");
+GetArray(firstmatrix);
+PrintArray(firstmatrix);
+
+Console.WriteLine();
+
+Console.WriteLine("Вторая матрица");
+GetArray(secondmatrix);
+PrintArray(secondmatrix);
+Console.WriteLine();
+if (firstmatrix.GetLength(0) != secondmatrix.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < firstmatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondmatrix.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < firstmatrix.GetLength(1); k++)
+        {
+            resultArray[i, j] += firstmatrix[i, k] * secondmatrix[k, j];
+        }
+    }
+}
+
+
+Console.WriteLine("Произведение двух матриц");
+PrintArray(resultArray);
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string value = Console.ReadLine();
+    int val = 0;
+    while ((int.TryParse(value, out val)) != true)
+    {
+        Console.Write("Неверный ввод. Повторите: ");
+        value = Console.ReadLine();
+
+    }
+
+    return val;
+}
+
+
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
